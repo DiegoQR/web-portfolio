9e3b168bb736c16276604eea09be4e563367a911
Diego QR <diegoqr2000@gmail.com>
2025-02-28 11:39:20 -0400

    Installed tailwind dependencies with vite

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import "./NavBar.css";
 
 function NavBar() {
   return (
     <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </nav>
   )
 }
