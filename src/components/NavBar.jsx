54637228a33280dc9fc8917d46c959e5768279ba
DiegoQR <diegoqr2000@gmail.com>
2025-03-05 20:44:55 -0400

    First version of the navbar implemented

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+    <nav className="bg-sky-950 h-16 flex justify-between items-center px-10 text-white">
+      <h1 className="text-3xl font-bold">DiegoDev.</h1>
+      <div className="flex gap-10">
+        <a className="text-lg"href="#home">Home</a>
+        <a className="text-lg" href="#about">About Me</a>
+        <a className="text-lg" href="#experience">Experience</a>
+        <a className="text-lg" href="#projects">Projects</a>
+        <a className="text-lg" href="#education">Education</a>
+        <a className="text-lg" href="#contact">Contact</a>
+      </div>
     </nav>
   )
 }
