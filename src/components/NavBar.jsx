6ebef146788b19fc0a4a209e7c10ffa4252cb8e6
DiegoQR <diegoqr2000@gmail.com>
2025-03-07 22:22:57 -0400

    Added first version of header page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 function NavBar() {
   return (
-    <nav className="bg-sky-950 h-16 flex justify-between items-center px-10 text-white">
-      <h1 className="text-3xl font-bold">DiegoDev.</h1>
-      <div className="flex gap-10">
-        <a className="text-lg"href="#home">Home</a>
-        <a className="text-lg" href="#about">About Me</a>
-        <a className="text-lg" href="#experience">Experience</a>
-        <a className="text-lg" href="#projects">Projects</a>
-        <a className="text-lg" href="#education">Education</a>
-        <a className="text-lg" href="#contact">Contact</a>
+    <nav className="bg-sky-950 h-16 flex justify-between items-center px-10 text-white sticky top-0 z-50 py-5 overflow-hidden">
+      <a href="#home">
+        <h1 className="text-3xl font-bold">DiegoDev.</h1>
+      </a>
+      <div className="flex gap-10 text-lg">
+        <a href="#home">Home</a>
+        <a href="#about">About Me</a>
+        <a href="#experience">Experience</a>
+        <a href="#projects">Projects</a>
+        <a href="#education">Education</a>
+        <a href="#contact">Contact</a>
       </div>
     </nav>
   )
