99153c91858bc73b6946681a082b507d8fc3e066
DiegoQR <diegoqr2000@gmail.com>
2025-03-11 23:26:29 -0400

    Improved navbar for mobile screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from 'react-dom/client'
+import { MantineProvider } from "@mantine/core";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
