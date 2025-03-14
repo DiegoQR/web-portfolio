f1cd4bca04ced07939207c66fd493b3e15a1d98b
DiegoQR <diegoqr2000@gmail.com>
2025-03-13 22:33:18 -0400

    Changed logic of the label element

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,12 +4,11 @@ import DevProject from './DevProject';
 function Projects() {
     const dataProjects = [
         {
-            urlImage: "https://i.imgur.com/ktLC9dO.png",
-            title: "Project 1",
-            description: "Brief description of the project",
-            technologies: ["HTML", "CSS", "Javascript"],
-            urlProject: "https//google.com",
-            urlRepository: "https://github.com",
+            urlImage: "https://res.cloudinary.com/dho0rif9d/image/upload/v1741906694/web-portfolio/imagen_2025-03-13_185810809_p2v0cx.png",
+            title: "Niños con Valor (NCV)",
+            description: "A university project in which we developed a system for a child foster NGO that helps them keep records of the children and caregivers in their care. ",
+            technologies: ["HTML", "CSS", "Javascript", "React", ".NET", "Git"],
+            urlRepository: "https://github.com/UCB-TallerDeDesarrollo/NCV.git",
         },
         {
             urlImage: "https://i.imgur.com/ktLC9dO.png",
