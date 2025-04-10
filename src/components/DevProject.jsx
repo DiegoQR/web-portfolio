d21184db39422ecf83b98e30c1d76d0866cb360e
DiegoQR <diegoqr2000@gmail.com>
2025-04-10 18:03:41 -0400

    Refactor DevProject component to use external tech data file for technology labels

--- src/components/DevProject.jsx
+++ src/components/DevProject.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import Label from './Label';
+import { techData as techLabelsDictionary } from '../data/TechData.jsx';
 
 function DevProject(props) {
-    const techLabelsDictionary= {
-        "HTML": { icon: <i className="fa-brands fa-html5"></i>, color: "bg-red-500"},
-        "CSS": { icon: <i className="fa-brands fa-css3"></i>, color: "bg-blue-500"},
-        "Javascript": { icon: <i className="fa-brands fa-js"></i>, color: "bg-yellow-500"},
-        "React": { icon: <i className="fa-brands fa-react"></i>, color: "bg-blue-700"},
-        "Node": { icon: <i className="fa-brands fa-node"></i>, color: "bg-teal-500"},
-        "Python": { icon: <i className="fa-brands fa-python"></i>, color: "bg-amber-600"},
-        ".NET": { icon: <i className="fa-solid fa-globe"></i>, color: "bg-purple-500"},
-        "SQL": { icon: <i className="fa-solid fa-database"></i>, color: "bg-lime-500"},
-        "MongoDB": { icon: <i className="fa-solid fa-server"></i>, color: "bg-green-500"},
-        "Git": { icon: <i className="fa-brands fa-square-git"></i>, color: "bg-slate-950"},
-    }
+
+
     const project = props.project;
     return (<>
         <div className="relative flex w-full flex-col rounded-xl bg-blue-dark shadow-md intersect:motion-preset-rebound-right lg:w-30/100 ">
